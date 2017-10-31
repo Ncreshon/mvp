d9cd65940097d0a3a18ed23a72b8b7aec44c2fbd
Nicole Creshon <ncreshon@gmail.com>
2017-10-31 17:07:12 -0500

    access to search input

---
+++ public/controllers/search.js
@@ -0,0 +1,12 @@
+// const http = require('http');
+
+// const keys = require('../config.js')
+
+const url = 'http://food2fork.com/api/search';
+
+angular.module("mvpApp",).controller("searchCtrl", ['$scope', function($scope) {
+   $scope.text = ''
+   $scope.submit = function(){
+  }
+}]);
+
